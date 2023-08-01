6f17c0cdea7e1eea9950d48b5e346d1c5f858969
srosama <srosamazaid@gmail.com>
2023-08-01 18:28:34 +0300

    Fix password func

--- backend/password_generator.py
+++ backend/password_generator.py
@@ -1,20 +1,31 @@
 import random
 import string
 
-def generate_password(length):
-    uppercase_letters = string.ascii_uppercase
-    lowercase_letters = string.ascii_lowercase
-    digits = string.digits
-    punctuation = string.punctuation
-    
-    # Combine the character sets into one set of all possible characters
-    all_characters = uppercase_letters + lowercase_letters + digits + punctuation
-    
-    # Generate a random password using the defined character set
-    password = ''.join(random.choice(all_characters) for i in range(length))
-    
+def generate_password(passwordLength=8, IncludeLowerCaseCharters=True, IncludeUpperCaseCharters=True, IncludeSymbols=True, IncludeNumbers=True, ExcludeSimilarCharacters=False, ExcludeAmbiguousCharacters=False):
+    if passwordLength not in [8, 16, 24]:
+        raise ValueError("Invalid password length. It should be one of [8, 16, 24].")
+
+    characters = ""
+    if IncludeLowerCaseCharters:
+        characters += string.ascii_lowercase
+    if IncludeUpperCaseCharters:
+        characters += string.ascii_uppercase
+    if IncludeSymbols:
+        characters += string.punctuation
+    if IncludeNumbers:
+        characters += string.digits
+
+    if ExcludeSimilarCharacters:
+        characters = ''.join(c for c in characters if c not in '1lIo0Oo')
+
+    if ExcludeAmbiguousCharacters:
+        characters = ''.join(c for c in characters if c not in '{}[]()/\'"`~,;:.<>')
+
+    if len(characters) == 0:
+        raise ValueError("No character types selected. Please include at least one character type.")
+
+    password = ''.join(random.choice(characters) for _ in range(passwordLength))
     return password
 
-
-password = generate_password(12)
+password = generate_password(passwordLength=24, IncludeLowerCaseCharters=False, IncludeUpperCaseCharters=False, IncludeSymbols=True, IncludeNumbers=True, ExcludeSimilarCharacters=True, ExcludeAmbiguousCharacters=True)
 print(password)
--- backend/routes.py
+++ backend/routes.py
@@ -1,4 +1,5 @@
 from . import app
+from .password_generator import generate_password
 import os
 import json
 from flask import jsonify, request, make_response, abort, url_for 
@@ -6,10 +7,17 @@ from flask import jsonify, request, make_response, abort, url_for
 
 @app.route('/', methods=['GET', 'POST'])
 def generate_Password():
-    greeting = 'Create Secure, Strong Password Fast And Easy Create a POST request With This Inputs: (passwordLength  IncludeSymbols  IncludeNumbers   IncludeLowerCaseCharters  IncludeUpperCaseCharters   ExcludeSimilarCharacters  ExcludeAmbiguousCharaters)'
+    greeting = 'Create Secure, Strong Password Fast And Easy Create a POST request With This Inputs: (passwordLength,  IncludeSymbols,  IncludeNumbers,   IncludeLowerCaseCharters,  IncludeUpperCaseCharters,   ExcludeSimilarCharacters,  ExcludeAmbiguousCharaters)'
     
     if request.method == 'POST':    
-        os = request.json
-        return json.dumps(os)
+        passwordData = request.json
 
+        passwordLength = passwordData['passwordLength'] 
+        IncludeSymbols = passwordData['IncludeSymbols'] 
+        IncludeNumbers = passwordData['IncludeNumbers'] 
+        IncludeLowerCaseCharters = passwordData['IncludeLowerCaseCharters'] 
+        IncludeUpperCaseCharters = passwordData['IncludeUpperCaseCharters'] 
+        ExcludeSimilarCharacters = passwordData['ExcludeSimilarCharacters'] 
+        ExcludeAmbiguousCharaters = passwordData['ExcludeAmbiguousCharaters'] 
+        # generate_password()
     return json.dumps(greeting)
